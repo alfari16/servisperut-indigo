c03f626b743b5d38b99666e5d5776c40167875c1
alfari16 <alif.anshory@yahoo.com>
2017-10-06 22:12:07 +0700

    detail produk

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ var idx=0;
 var form = $('.form-group');
 var windows = $(window);
 var wWith = windows.width();
-
+var body = $('body');
+var a;
+var counter = 0;
 
 function move() {
   if(idx === 2){
@@ -33,10 +35,9 @@ $(document).ready(function() {
   });
   $('.text-animate p').addClass('animate');
   $('.limiter').addClass('animate');
-  if($('body').hasClass('index')){
+  if(body.hasClass('index')){
     $('.owl-carousel').owlCarousel({
       nav:false,
-      indicator:false,
       dots:false,
       loop:true,
       margin:0,
@@ -44,6 +45,7 @@ $(document).ready(function() {
       autoplay:true,
       autoplayTimeout:4000,
       autoplayHoverPause:false,
+      lazyLoad:true,
       responsive:{
         0:{
           items:2,
@@ -55,7 +57,39 @@ $(document).ready(function() {
         }
       }
     });
+  } else if (body.hasClass('produk')){
+    var ratingDOM = $('.rating');
+    var rating = parseInt(ratingDOM.attr('data-rating'));
+    var sliderProduk = $('.produk .img-slide');
+    var sliderProdukChildLength = sliderProduk.children().eq(0).innerWidth();
+    sliderProduk.width(sliderProdukChildLength * 3).children().width(sliderProdukChildLength);
+    if($(this).width()>768){
+      sliderProduk.hover(function () {
+        sliderProduk.addClass('open');
+        clearInterval(a);
+      }).mouseleave(function () {
+        sliderProduk.removeClass('open');
+        slide();
+      });
+    }
+    ratingDOM.children().each(function(i){
+      if(i>=rating){
+        return false;
+      }
+      $(this).addClass('colored');
+    });
+    slide();
+    sliderProduk.find('figure').click(function(){
+      $('.produk .lightbox img').attr('src', $(this).find('img').attr('src'));
+      lightbox();
+    });
+    $('.lightbox').click(function(e){
+      if ($(e.target).hasClass('lightbox') || $(e.target).hasClass('tutup')){
+        lightbox();
+      }
+    });
   }
+  
 }).scroll(function(){
   var wScroll = $(this).scrollTop();
   var navbar = $('.navbar.navbar-default');
@@ -65,6 +99,19 @@ $(document).ready(function() {
 windows.resize(function(){
   if(wWith != windows.width()){
     location.reload(true);
-    console.log("jsdjskd");
   }
 });
+
+function slide(){
+  a= setInterval(function(){
+    if(counter<-200) counter=0;
+    $('.produk .img-slide').css({
+      left:counter+'%',
+    });
+    counter-=100;
+  },3000);
+}
+function lightbox(){
+  $('.lightbox').toggleClass('hidden');
+  $('.wrapper-all').toggleClass('blur');
+}
